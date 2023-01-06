2cc775748d7bd85bd5659bcda19eabc7d600e428
Anthony Berland <anthony.berland@gmail.com>
2023-01-06 15:27:54 -0700

    Update streamlit_app.py
    corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-stremalit.header('Header example')
+streamlit.header('Header example')
 streamlit.text('Text1 example')
 streamlit.text('Text2 example')
 streamlit.text('Text3 example')
