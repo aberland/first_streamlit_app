b54941b4b5c5eed5b26cf23b5fb719c6c71e1042
Anthony Berland <anthony.berland@gmail.com>
2023-01-06 15:27:18 -0700

    Update streamlit_app.py
    added header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit 
 
 streamlit.title('My parents new healthy diner')
+
+stremalit.header('Header example')
+streamlit.text('Text1 example')
+streamlit.text('Text2 example')
+streamlit.text('Text3 example')
