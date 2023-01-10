14defaf47db9e4ee0efe10a4879a02da71658ff1
Anthony Berland <anthony.berland@gmail.com>
2023-01-10 10:35:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,4 @@ streamlit.dataframe(my_data_row)
 new_fruit_choice = streamlit.text_input('What fruit would you like to add?')
 if new_fruit_choice != '':
   my_cur.execute("insert into fruit_load_list (fruit_name) values ('" + new_fruit_choice + "')")
-my_cur.execute("delete from fruit_load_list where fruit_name = 'pear'")
 
